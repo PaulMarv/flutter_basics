13b0af59cad2820e3fe84f24fadb259778b025a0
PaulMarv <mpsalako@gmail.com>
2023-05-31 17:15:32 +0100

    done basic setup and navigation

---
+++ android/app/src/main/kotlin/com/example/flutt1_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutt1_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(debugShowCheckedModeBanner:false, 
+    home: MyAppExt(),
+    );
+  }
+}
+
+class MyAppExt extends StatefulWidget {
+  const MyAppExt({super.key});
+
+  @override
+  State<MyAppExt> createState() => _MyAppExtState();
+}
+
+class _MyAppExtState extends State<MyAppExt> {
+  String buttonName = 'Click';
+  int currentIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('App title'),
+      ),
+      body: Center(
+        child: currentIndex == 0
+            ? Container(
+                width: double.infinity,
+                height: double.infinity,
+                color: Colors.red,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        onPrimary: Colors.white,
+                        primary: Colors.orange,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          buttonName = 'Now clicked !';
+                        });
+                      },
+                      child: Text(buttonName),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (BuildContext context) => const NextPage(),
+                          ),
+                        );
+                      },
+                      child: const Text('Next Page'),
+                    ),
+                  ],
+                ),
+              )
+            : GestureDetector(child: Image.asset('images/profile.jpg'),),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            label: 'Home',
+            icon: Icon(Icons.home),
+          ),
+          BottomNavigationBarItem(
+            label: 'settings',
+            icon: Icon(Icons.settings),
+          ),
+        ],
+        currentIndex: currentIndex,
+        onTap: (int index) {
+          setState(() {
+            currentIndex = index;
+          });
+        },
+      ),
+    );
+  }
+}
+
+class NextPage extends StatelessWidget {
+  const NextPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
